2ea1c6ee3411d176e4d572864aeed81563998c1b
PSY <6howcome9@gmail.com>
2022-10-01 12:07:09 +0900

    feat: footer, go back, stateful nav toggle

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,22 @@
 window.onload = () => {
-  if (window.innerWidth < 920) {
-    const $toggle = document.querySelector('#nav_toggle');
+  const $toggle = document.querySelector('#nav_toggle');
+
+  if (localStorage.getItem('isExpanded')) {
+    $toggle.checked = true;
+  } else {
     $toggle.checked = false;
   }
+
+  if (window.innerWidth < 920) {
+    $toggle.checked = false;
+    localStorage.setItem('isExpanded', false);
+  }
 };
+
+function toggleHandler($toggle) {
+  if ($toggle.checked) {
+    localStorage.setItem('isExpanded', true);
+  } else {
+    localStorage.setItem('isExpanded', false);
+  }
+}
